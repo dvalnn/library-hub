0de37ec8226b37930d90f99a16856e9bd9ac2a33
miguel <miguel.silva.amorim@gmail.com>
2024-09-25 14:05:09 +0100

    botão novo de submit

--- frontend/src/app/mainWindow.jsx
+++ frontend/src/app/mainWindow.jsx
@@ -6,8 +6,8 @@ import Agent from "./agent.jsx";
 //TODO: Fazer Scroll window em .resultsContainer
 //TODO: Tratar de toda a parte direita dos Registos
 //TODO: Arranjar botão submit!  
-//TODO: 	1. Tá buggy e o texto tá a dar overlay.
-//TODO: 	2. meté-lo a aparecer apenas quando há resultados de pesquisa
+//TODO: 	1. meté-lo a aparecer apenas quando há resultados de pesquisa
+//TODO: 	Hook para AgentsList para sacar a element.lenght != mostro <SubmitBtn/>
 
 
 function MainWindow({ searchArgs }) {
@@ -16,46 +16,48 @@ function MainWindow({ searchArgs }) {
 			<div id="leftWindow" className="resultWindow">
 				<h1 className="title">Resultados da Pesquisa</h1>
 				<AgentList searchArgs={searchArgs} btnType={1} />
-				<SubmitBtn leftRight={1}/>
+				<SubmitBtn SubmtDelete={1} />
 			</div>
-			<div id="rightWindow" className="resultWindow"> 
+			<div id="rightWindow" className="resultWindow">
 				<h1 className="title">Registos</h1>
 			</div>
 		</div>
 	);
 }
 
-function SubmitBtn({ leftRight }) {
-	if (leftRight === 1) {
+function SubmitBtn({ SubmtDelete }) {
+	if (SubmtDelete === 1) {
 		return (
-			<button className="submit" id="create">
-				<span>
-					Criar Registo
-					<svg fill="none" viewBox="0 0 24 24"><g strokeWidth="0"></g><g strokeLinejoin="round" strokeLinecap="round"></g><g> <path fill="#ffffff" d="M20.33 3.66996C20.1408 3.48213 19.9035 3.35008 19.6442 3.28833C19.3849 3.22659 19.1135 3.23753 18.86 3.31996L4.23 8.19996C3.95867 8.28593 3.71891 8.45039 3.54099 8.67255C3.36307 8.89471 3.25498 9.16462 3.23037 9.44818C3.20576 9.73174 3.26573 10.0162 3.40271 10.2657C3.5397 10.5152 3.74754 10.7185 4 10.85L10.07 13.85L13.07 19.94C13.1906 20.1783 13.3751 20.3785 13.6029 20.518C13.8307 20.6575 14.0929 20.7309 14.36 20.73H14.46C14.7461 20.7089 15.0192 20.6023 15.2439 20.4239C15.4686 20.2456 15.6345 20.0038 15.72 19.73L20.67 5.13996C20.7584 4.88789 20.7734 4.6159 20.7132 4.35565C20.653 4.09541 20.5201 3.85762 20.33 3.66996ZM4.85 9.57996L17.62 5.31996L10.53 12.41L4.85 9.57996ZM14.43 19.15L11.59 13.47L18.68 6.37996L14.43 19.15Z"></path> </g></svg>
-				</span>
-				<span>
-					De certeza?
-				</span>
-				<span>
-					Feito !
-					<svg fill="none" viewBox="0 0 24 24"><g strokeWidth="0"></g><g strokeLinejoin="round" strokeLinecap="round"></g><g> <path strokeLinecap="round" strokeWidth="2" stroke="#ffffff" d="M8.00011 13L12.2278 16.3821C12.6557 16.7245 13.2794 16.6586 13.6264 16.2345L22.0001 6"></path> <path fill="#ffffff" d="M11.1892 12.2368L15.774 6.63327C16.1237 6.20582 16.0607 5.5758 15.6332 5.22607C15.2058 4.87635 14.5758 4.93935 14.226 5.36679L9.65273 10.9564L11.1892 12.2368ZM8.02292 16.1068L6.48641 14.8263L5.83309 15.6248L2.6 13.2C2.15817 12.8687 1.53137 12.9582 1.2 13.4C0.868627 13.8419 0.95817 14.4687 1.4 14.8L4.63309 17.2248C5.49047 17.8679 6.70234 17.7208 7.381 16.8913L8.02292 16.1068Z" clipRule="evenodd" fillRule="evenodd"></path> </g></svg>
-				</span>
+			<button className="submit" id="RegBtn">
+				<svg viewBox="0 0 24 24" className="arr-2">
+					<path
+						d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+					></path>
+				</svg>
+				<span className="text">Registar</span>
+				<span className="circle"></span>
+				<svg viewBox="0 0 24 24" className="arr-1">
+					<path
+						d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+					></path>
+				</svg>
 			</button>
 		)
 	}
 	return (
-		<button className="submit" id="delete">
-			<span>
-				Eliminar Registo
-				<svg fill="none" viewBox="0 0 24 24"><g strokeWidth="0"></g><g strokeLinejoin="round" strokeLinecap="round"></g><g> <path fill="#ffffff" d="M20.33 3.66996C20.1408 3.48213 19.9035 3.35008 19.6442 3.28833C19.3849 3.22659 19.1135 3.23753 18.86 3.31996L4.23 8.19996C3.95867 8.28593 3.71891 8.45039 3.54099 8.67255C3.36307 8.89471 3.25498 9.16462 3.23037 9.44818C3.20576 9.73174 3.26573 10.0162 3.40271 10.2657C3.5397 10.5152 3.74754 10.7185 4 10.85L10.07 13.85L13.07 19.94C13.1906 20.1783 13.3751 20.3785 13.6029 20.518C13.8307 20.6575 14.0929 20.7309 14.36 20.73H14.46C14.7461 20.7089 15.0192 20.6023 15.2439 20.4239C15.4686 20.2456 15.6345 20.0038 15.72 19.73L20.67 5.13996C20.7584 4.88789 20.7734 4.6159 20.7132 4.35565C20.653 4.09541 20.5201 3.85762 20.33 3.66996ZM4.85 9.57996L17.62 5.31996L10.53 12.41L4.85 9.57996ZM14.43 19.15L11.59 13.47L18.68 6.37996L14.43 19.15Z"></path> </g></svg>
-			</span>
-			<span>
-				De certeza?
-			</span>
-			<span>
-				Feito !
-				<svg fill="none" viewBox="0 0 24 24"><g strokeWidth="0"></g><g strokeLinejoin="round" strokeLinecap="round"></g><g> <path strokeLinecap="round" strokeWidth="2" stroke="#ffffff" d="M8.00011 13L12.2278 16.3821C12.6557 16.7245 13.2794 16.6586 13.6264 16.2345L22.0001 6"></path> <path fill="#ffffff" d="M11.1892 12.2368L15.774 6.63327C16.1237 6.20582 16.0607 5.5758 15.6332 5.22607C15.2058 4.87635 14.5758 4.93935 14.226 5.36679L9.65273 10.9564L11.1892 12.2368ZM8.02292 16.1068L6.48641 14.8263L5.83309 15.6248L2.6 13.2C2.15817 12.8687 1.53137 12.9582 1.2 13.4C0.868627 13.8419 0.95817 14.4687 1.4 14.8L4.63309 17.2248C5.49047 17.8679 6.70234 17.7208 7.381 16.8913L8.02292 16.1068Z" clipRule="evenodd" fillRule="evenodd"></path> </g></svg>
-			</span>
+		<button className="submit" id="DeleteBtn">
+			<svg viewBox="0 0 24 24" className="arr-2">
+				<path
+					d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+				></path>
+			</svg>
+			<span className="text">Registar</span>
+			<span className="circle"></span>
+			<svg viewBox="0 0 24 24" className="arr-1">
+				<path
+					d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+				></path>
+			</svg>
 		</button>
 	)
 }
