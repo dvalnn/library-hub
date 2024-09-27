1ed3a4afc2d6d581f8c5aaa2465908a53274e266
miguel <miguel.silva.amorim@gmail.com>
2024-09-27 23:56:20 +0100

    new colors and pretty buttons

--- frontend/src/app/buttons.jsx
+++ frontend/src/app/buttons.jsx
@@ -21,27 +21,29 @@ function SubmitBtn({ BtnId, BtnTxt }) {
 function SelectButton({ isSelected, handleClick }) {
 
 	const plusButton = () => (
-		<svg className="selectIcon" viewBox="0 0 30 30">
+		<svg className="selectIcon" id="plus" viewBox="0 0 30 30">
 			<title>Selecionar</title>
 			<path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z" />
 		</svg>
 	);
 
 	const minusButton = () => (
-		<svg className="selectIcon" viewBox="0 0 30 30">
+		<svg className="selectIcon" id="minus" viewBox="0 0 30 30">
 			<title>Desselecionar</title>
 			<path d="M6.25 16.25H23.75V13.75H6.25V16.25Z" />
 		</svg>
 	);
 
 	const selectBtn = isSelected ? minusButton() : plusButton();
+	const btnID = isSelected ? "minusBtn" : "plusBtn";
 
 	return (
 		<button
 			type="button"
 			className="registBtn"
-			id="selectIcon"
+			id={btnID}
 			onClick={handleClick}>
+			<style>{`transition: transform 0.3s ease;`}</style>
 			{selectBtn}
 		</button >
 	);
