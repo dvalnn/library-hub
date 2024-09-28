04fe27592ec1a3710ab2979ae4f18be20db67d04
miguel <miguel.silva.amorim@gmail.com>
2024-09-28 17:35:03 +0100

    mainWindow final style and searchbar

--- frontend/src/app/buttons.jsx
+++ frontend/src/app/buttons.jsx
@@ -21,14 +21,14 @@ function SubmitBtn({ btnId, btnText, handleClick }) {
 function SelectButton({ isSelected, handleClick }) {
 	const plusButton = () => (
 		<svg className="selectIcon" id="plus" viewBox="0 0 30 30">
-			<title>Selecionar</title>
+			<title>Adicionar</title>
 			<path d="M13.75 23.75V16.25H6.25V13.75H13.75V6.25H16.25V13.75H23.75V16.25H16.25V23.75H13.75Z" />
 		</svg>
 	);
 
 	const minusButton = () => (
 		<svg className="selectIcon" id="minus" viewBox="0 0 30 30">
-			<title>Desselecionar</title>
+			<title>Remover</title>
 			<path d="M6.25 16.25H23.75V13.75H6.25V16.25Z" />
 		</svg>
 	);
