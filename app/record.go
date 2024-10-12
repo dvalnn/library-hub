c775aa3c77029cc92930b8cdf3b86c41122abd03
dvalinn <tiago.andre.amorim@gmail.com>
2024-10-12 01:50:19 +0100

    agent search

--- app/record.go
+++ app/record.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"errors"
 	"fmt"
 
 	"gorm.io/gorm"
@@ -51,4 +52,41 @@ func (r *Record) delete(db *gorm.DB) error {
 	}
 
 	return nil
+}
+
+const _RECORD_MAX_SEARCH_HITS = 200
+
+func recordSearchByDay(db *gorm.DB, day string) ([]*Record, error) {
+	var records []*Record
+	err := db.Where("date(updated_at) = '?' AND deleted_at IS NULL", day).Find(&records).Limit(_RECORD_MAX_SEARCH_HITS).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, fmt.Errorf("no records for day %s", day)
+		}
+		return nil, fmt.Errorf("internal server error: %w", err)
+	}
+
+	if len(records) == 0 {
+		return nil, fmt.Errorf("no records for day %s: %w", day, gorm.ErrRecordNotFound)
+	}
+
+	return records, nil
+}
+
+func recordSearchByDayFiltered(db *gorm.DB, day string, filter AgentKind) ([]*Record, error) {
+	var records []*Record
+	const query = "date(updated_at) = '?' AND deleted_at IS NULL"
+	err := db.Where(query, day).Preload("agents", "agent_kind = ?", filter).Find(&records).Limit(_RECORD_MAX_SEARCH_HITS).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, fmt.Errorf("no records for %s for day %s", filter, day)
+		}
+		return nil, fmt.Errorf("internal server error: %w", err)
+	}
+
+	if len(records) == 0 {
+		return nil, fmt.Errorf("no records for %s for day %s: %w", filter, day, gorm.ErrRecordNotFound)
+	}
+
+	return records, nil
 }
