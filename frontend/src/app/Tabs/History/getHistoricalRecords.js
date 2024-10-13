d8eb2a2a029a243f070a1f93c317d1b10d9ba9b7
dvalinn <tiago.andre.amorim@gmail.com>
2024-10-13 14:01:15 +0100

    date picker 1 day bug offset fixed

--- frontend/src/app/Tabs/History/getHistoricalRecords.js
+++ frontend/src/app/Tabs/History/getHistoricalRecords.js
@@ -3,7 +3,11 @@ import { SearchRecordsByDay } from "../../../../wailsjs/go/app/App.js";
 import AgentEnum from "../Common/agentEnum.js";
 
 async function getHistoricalRecords(date, filterStr) {
-	const day = date.toISOString().split("T")[0];
+	// Adjust the date to local timezone before converting to ISO string
+	const adjustedDate = new Date(
+		date.getTime() - date.getTimezoneOffset() * 60000,
+	);
+	const day = adjustedDate.toISOString().split("T")[0];
 	const filter = AgentEnum.getValue(filterStr);
 
 	try {
